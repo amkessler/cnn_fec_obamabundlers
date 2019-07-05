3b325eba4af02a63758a6ddd6da258077a3bc1fd
Aaron Kessler <kessler.aaron@gmail.com>
2019-07-05 00:47:25 -0400

    remove old analysis code no longer relevant

--- 02_namescoring_analysis.R
+++ 02_namescoring_analysis.R
@@ -114,7 +114,7 @@ joined <- inner_join(bundlers, unique_potential_matches, by = "matchstring")
 # ASSIGNING SCORES TO POSSIBLE MATCHES ####
 names(joined)
 
-final_for_check <- joined %>% 
+final_for_research <- joined %>% 
   mutate(
     match_type = case_when(
          bundler_last == contributor_last_name & 
@@ -134,79 +134,6 @@ final_for_check <- joined %>%
   select(match_type, everything()) 
 
 
-final_for_check %>% 
+final_for_research %>% 
   count(match_type)
 
-
-#### FORMER ANALYSIS SCRIPTS ####
-# 
-# 
-# #add candidate names
-# targetlist_wcandname <- left_join(targetlist, candnames, by = c("filer_committee_id_number" = "fec_committee_id")) %>% 
-#   select(cand_name = name, everything())  
-# 
-# #output
-# write_csv(targetlist_wcandname, "output/targetlist_wcandname.csv")
-# saveRDS(targetlist_wcandname, "processed_data/targetlist_wcandname.rds")
-# 
-# 
-# ### FIND UNIQUE NAME/ADDRESS VARIATIONS ####
-# 
-# #load from rds file above to allow for local analysis 
-# #without having to hit db server if desired
-# targetlist_wcandname <- readRDS("processed_data/targetlist_wcandname.rds")
-# 
-# names(targetlist_wcandname)
-# 
-# # targetlist_uniques <- targetlist_wcandname %>% 
-# #   select(cand_name,
-# #          contributor_last_name,
-# #          contributor_first_name,
-# #          contributor_middle_name,
-# #          contributor_suffix,
-# #          contributor_street_1,
-# #          contributor_street_2,
-# #          contributor_city,
-# #          contributor_state,
-# #          zip5,
-# #          contributor_employer,
-# #          contributor_occupation
-# #         ) %>% 
-# #   unique()
-# 
-# #group by unique and contrib amounts
-# targetlist_uniques <- targetlist_wcandname %>% 
-#   group_by(cand_name,
-#          contributor_last_name,
-#          contributor_first_name,
-#          contributor_middle_name,
-#          contributor_suffix,
-#          contributor_street_1,
-#          contributor_street_2,
-#          contributor_city,
-#          contributor_state,
-#          zip5,
-#          contributor_employer,
-#          contributor_occupation
-#   ) %>% 
-#   summarise(
-#     sum_contribs_period = sum(contribution_amount),
-#     aggregate_contribs_cycle = max(contribution_aggregate))
-# 
-# 
-# # filter to just democrats
-# targetlist_uniques %>% 
-#   group_by(cand_name) %>% 
-#   summarise(n())
-# 
-# 
-# 
-# #group by states
-# targetlist_uniques %>% 
-#   count(contributor_state) %>% 
-#   arrange(desc(n))
-# 
-# #group by occupations
-# targetlist_uniques %>% 
-#   count(contributor_occupation) %>% 
-#   arrange(desc(n))
