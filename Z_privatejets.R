3d8b241c4b2c7589d5f1ba65c790dc1fff4c27ab
Aaron Kessler <kessler.aaron@gmail.com>
2019-07-06 17:38:33 -0400

    pull travel related expenses

--- Z_privatejets.R
+++ Z_privatejets.R
@@ -23,21 +23,84 @@ expends_db <- expends_db %>%
 #   filter(filer_committee_id_number == "C00213652")
 
 
-# 
-# #pull candidate table from postgres db
-# cand_db <- tbl(con, "cycle_2020_candidate")
-# 
-# #filter only for presidential
-# candnames <- cand_db %>% 
-#   filter(district == "US") %>% 
-#   select(name, fec_committee_id) %>% 
-#   collect()
-# 
-# #grab prez committee ids
-# prez_cmte_ids <- candnames %>% pull(fec_committee_id)
-# 
-# #filter the contribs table by them
-# prez_contribs <- contribs_db %>% 
-#   filter(filer_committee_id_number %in% prez_cmte_ids)
+#pull candidate table from postgres db
+cand_db <- tbl(con, "cycle_2020_candidate")
+
+#filter only for presidential
+candnames <- cand_db %>%
+  filter(district == "US") %>%
+  select(name, fec_committee_id) %>%
+  collect()
+
+#grab prez committee ids
+prez_cmte_ids <- candnames %>% pull(fec_committee_id)
+
+#filter the contribs table by them and then collect locally
+prez_expends <- expends_db %>%
+  filter(filer_committee_id_number %in% prez_cmte_ids) %>% 
+  collect()
+
+#join to add candidate name to table
+prez_expends <- prez_expends %>% 
+  left_join(candnames, by = c("filer_committee_id_number" = "fec_committee_id")) %>% 
+  select(name, everything())
+
+#date format and derived columns
+prez_expends$expenditure_date <- ymd(prez_expends$expenditure_date)
+prez_expends$expenditure_year <- year(prez_expends$expenditure_date)
+prez_expends$expenditure_month <- month(prez_expends$expenditure_date)
+prez_expends$expenditure_day <- day(prez_expends$expenditure_date)
 
 
+prez_expends %>% 
+  filter(expenditure_year == 2019) %>% 
+  group_by(status) %>% 
+  summarise(n(), sum(expenditure_amount))
+
+prez_expends %>% 
+  filter(expenditure_year == 2019) %>% 
+  group_by(name, status) %>% 
+  summarise(n(), sum(expenditure_amount))
+
+
+prez_expends %>% 
+  filter(expenditure_year == 2019,
+         status == "ACTIVE") %>% 
+  group_by(name) %>% 
+  summarise(num = n(), tot_amt = sum(expenditure_amount)) %>% 
+  arrange(desc(tot_amt))
+
+
+# attempt to isolate travel expenses ####
+prez_expends <- prez_expends %>% 
+  mutate(
+    expenditure_purpose_descrip = str_squish(str_to_upper(expenditure_purpose_descrip)),
+    payee_organization_name = str_squish(str_to_upper(payee_organization_name))
+  )
+
+prez_travel <- prez_expends %>% 
+  filter(expenditure_year == 2019,
+         str_detect(expenditure_purpose_descrip, "TRAVEL") |
+           str_detect(expenditure_purpose_descrip, "TRANSPORTATION") |
+           str_detect(expenditure_purpose_descrip, "AIRFARE") |
+           str_detect(expenditure_purpose_descrip, "AIRLINE") |
+           str_detect(expenditure_purpose_descrip, "PLANE")
+           ) 
+
+#see the desc variations
+prez_travel %>% 
+  count(expenditure_purpose_descrip) 
+
+#check amounts for active vs. memo
+prez_travel %>% 
+  group_by(status) %>% 
+  summarise(n(), sum(expenditure_amount))
+
+
+#look for possible airline mentions as payee
+air_search <- prez_travel %>% 
+         filter(str_detect(payee_organization_name, "AIR"))
+
+air_search %>% 
+  count(payee_organization_name) %>% 
+  View()
